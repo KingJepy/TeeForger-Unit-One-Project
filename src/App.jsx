bf600fff42da8fb2c18f2c2f4a13ce9c99453d2a
Jepy <jmoslander15@gmail.com>
2025-07-16 19:38:23 -0500

    added react-router-dom and created home page route

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 
 import './App.css'
 import Footer from './components/Footer';
-import Header from './components/Header'
+import Header from './components/Header';
 import HomePage from './components/HomePage';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 function App() {
 
   return (
     <div className='entire-page'>
       <Header />
-      <div>
-        <HomePage />
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<HomePage />}/>
+
+        </Routes>
+      </BrowserRouter>
       <Footer />
     </div>
   )
